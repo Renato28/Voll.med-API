65a95a7bd96a1be72eb9fe9ffb742ee7dceeb97c
Renato Nobrega <renatonobrega87@gmail.com>
2023-04-11 17:45:19 -0300

    adicionando projeto

---
+++ src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
@@ -0,0 +1,15 @@
+package med.voll.api.domain.consulta;
+
+import med.voll.api.domain.consulta.Consulta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+
+@Repository
+public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+
+    boolean existsByMedicoIdAndDataAndMotivoCancelamentoIsNull(Long idMedico, LocalDateTime data);
+
+    boolean existsByPacienteIdAndDataBetween(Long idPaciente, LocalDateTime primeiroHorario, LocalDateTime ultimoHorario);
+}
---
+++ src/main/java/med/voll/api/domain/paciente/PacienteRepository.java
@@ -0,0 +1,13 @@
+package med.voll.api.domain.paciente;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+    Page<Paciente> findAllByAtivoTrue(Pageable paginacao);
+
+    Boolean findAtivoById(Long id);
+}
